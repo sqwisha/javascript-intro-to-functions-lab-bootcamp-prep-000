88e1140b3158727238ad1de8202cbd0cd314d538
Sasha Hart <sashanicolehart@gmail.com>
2018-08-23 00:42:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ function logWhister(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === "I love you, Grandma.") {
+  if(string === string.toLowerCase()) {
+    return `I can't hear you!`;
+  }
+  else if(string === string.toUpperCase()) {
+    return `YES INDEED!`;
+  }
+  else (string === "I love you, Grandma.") {
     return `I love you, too.`;
   }
 }
