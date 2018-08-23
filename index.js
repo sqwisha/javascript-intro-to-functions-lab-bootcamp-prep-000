a35fbecf72c74c6659b2048c6c4864455444329e
Sasha Hart <sashanicolehart@gmail.com>
2018-08-23 00:37:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ function whisper(string) {
   return string.toLowerCase();
 }
 
+function logShout(string) {
+  console.log(string.toUpperCase);
+}
+
+function logWhister(string) {
+  console.log(string.toLowerCase);
+}
